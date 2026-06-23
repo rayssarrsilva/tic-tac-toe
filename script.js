17c0c02ad07f582edb4ee1fc07927da966fcd8c6
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-06-23 18:10:03 -0300

    Return the createplayer function test
    return in the console the test of instancies

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const gameboard = {
 
 };
 
-function player(name) {
-    const points = 0;
+function createPlayer(name) {
+    let points = 0;
 
     const getWins = () => points;
     const giveWins = () => {
-        points++
+        points++;
     };
 
     return {getWins, giveWins, name};
 }
+
+const p1 = createPlayer("rayssa");
+const p2 = createPlayer("roberta");
+
+p1.giveWins();
+p1.giveWins();
+
+console.log(p1.getWins());
 
 const controlFlow = {
 
