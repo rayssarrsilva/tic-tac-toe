fb066e5fac14b4d76ccf2ba019faa8328b7e02a8
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-06-24 12:25:25 -0300

    Create a function to check the winner and more
    add a function to start the prompt game, add checkwinner function and
    return if it have a empass

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const gameboard = {
-
-};
-
 function createPlayer(name) {
     let points = 0;
 
@@ -13,14 +9,66 @@ function createPlayer(name) {
     return {getWins, giveWins, name};
 }
 
-const p1 = createPlayer("rayssa");
-const p2 = createPlayer("roberta");
-
-p1.giveWins();
-p1.giveWins();
-
-console.log(p1.getWins());
-
-const controlFlow = {
+function controlFlow() {
 
 };
+
+function boardGame() {
+    let gameBoard = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    const winPattern = [
+        [0, 1, 2],
+        [3, 4, 5], 
+        [6, 7, 8],
+
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    let i = 0;
+    console.log("The board is 3x3");
+
+    while (i <= 8){
+        let position1 = prompt("Player 1: Inform the Line and Column. Example: 1 3");
+        let position2 = prompt("Player 2: Inform the Line and Column. Example: 1 3");
+
+        let [line, col] = position1.split(" ").map(Number);
+        let [line1, col1] = position2.split(" ").map(Number);
+
+        line = line -1;
+        col = col - 1;
+        line1 = line - 1;
+        col1 = col1 - 1;
+
+        if (gameBoard[line][col] === ""){
+            gameBoard[line][col] = "X";
+            console.log(gameBoard);
+
+            winner = checkWinner(gameBoard);
+            if (winner) {
+                alert(`${winner} venceu`);
+            } else if (!winner && gameBoard.every(cell => cell !== "")){
+                alert("Empass");
+            }
+
+            i++
+        } else {
+            alert("This position is marked, choose again");
+        }
+    };
+
+    function checkWinner(gameBoard){
+        for (let [a, b, c] of winPattern){
+            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
+                return gameBoard[c];
+            }
+        }
+    };
+
+};
+
+boardGame();
